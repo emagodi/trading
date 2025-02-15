ecf35e65e7dc95316f9b3f870f6256f0e0c4bc92
emagodi <magodiedwin@gmail.com>
2025-02-15 14:55:41 +0200

    update main class to add have springboot and vertx

--- src/main/kotlin/za/co/varl/trading/TradingApplication.kt
+++ src/main/kotlin/za/co/varl/trading/TradingApplication.kt
@@ -1,10 +1,62 @@
 package za.co.varl.trading
 
+import io.vertx.core.Vertx
+import io.vertx.ext.web.Router
+import io.vertx.ext.web.handler.BodyHandler
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cache.annotation.EnableCaching
+import org.springframework.context.annotation.Bean
+import org.springframework.cache.CacheManager
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager
+import za.co.varl.trading.config.JwtUtil
+import za.co.varl.trading.controller.AuthController
+import za.co.varl.trading.controller.OrderController
+import za.co.varl.trading.service.AuthService
+import za.co.varl.trading.service.OrderService
+import za.co.varl.trading.service.RateLimitService
 
 @SpringBootApplication
-class TradingApplication
+@EnableCaching // Enable caching in the Spring application
+class TradingApplication {
+
+	@Bean
+	fun vertxRouter(authService: AuthService, orderService: OrderService, rateLimitService: RateLimitService): Router {
+		val vertx = Vertx.vertx()
+		val router = Router.router(vertx)
+
+		// Create an instance of JwtUtil
+		val jwtUtil = JwtUtil("586B633834416E396D7436753879382F423F4428482B4C6250655367566B5970")
+
+		// Add BodyHandler to parse the body of incoming requests
+		router.route().handler(BodyHandler.create())
+
+		// Set up the AuthController routes
+		val authController = AuthController(authService, jwtUtil)
+		authController.setupRoutes(router)
+
+		// Set up the OrderController routes
+		val orderController = OrderController(orderService, vertx, jwtUtil, rateLimitService) // Pass rateLimitService
+		orderController.setupRoutes(router)
+
+		// Start the Vert.x HTTP server
+		vertx.createHttpServer()
+			.requestHandler(router)
+			.listen(8080) { result ->
+				if (result.succeeded()) {
+					println("Vert.x server is now listening on port 8080!")
+				} else {
+					println("Failed to bind Vert.x server: ${result.cause()}")
+				}
+			}
+		return router
+	}
+
+	@Bean
+	fun cacheManager(): CacheManager {
+		return ConcurrentMapCacheManager("orders", "trades") // Define your cache names here
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<TradingApplication>(*args)
