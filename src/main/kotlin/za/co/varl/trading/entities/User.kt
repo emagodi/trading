5afbd440555c9579b797961a8aec6524f0a006ac
emagodi <magodiedwin@gmail.com>
2025-02-15 14:43:30 +0200

    Add user entity

---
+++ src/main/kotlin/za/co/varl/trading/entities/User.kt
@@ -0,0 +1,25 @@
+package za.co.varl.trading.entities
+
+import za.co.varl.trading.enums.EmploymentStatus
+import za.co.varl.trading.enums.Purpose
+import za.co.varl.trading.enums.Role
+import za.co.varl.trading.enums.SourceOfFunds
+
+data class User(
+    val id: Long,  // Ensure this property exists
+    val password: String,
+    val role: Role,
+    val firstName: String,
+    val lastName: String,
+    val dateOfBirth: String,
+    val residentialCountry: String,
+    val identityIssuingCountry: String,
+    val identityType: String,
+    val identityNumber: String,
+    val identityExpiryDate: String,
+    val cellNumber: String,
+    val email: String,
+    val purpose: Purpose,
+    val employmentStatus: EmploymentStatus,
+    val sourceOfFunds: SourceOfFunds
+)
