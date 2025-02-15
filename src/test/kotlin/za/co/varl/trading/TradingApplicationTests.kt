4dc11c29ebd2f4e7b26c402116024d50d2179217
emagodi <magodiedwin@gmail.com>
2025-02-15 13:44:43 +0200

    Springboot start project

---
+++ src/test/kotlin/za/co/varl/trading/TradingApplicationTests.kt
@@ -0,0 +1,13 @@
+package za.co.varl.trading
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TradingApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
