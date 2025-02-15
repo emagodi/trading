75cacc46e7eb21838d7346b63d2f401dea192f9d
emagodi <magodiedwin@gmail.com>
2025-02-15 14:53:48 +0200

    add OrderService

---
+++ src/main/kotlin/za/co/varl/trading/service/OrderService.kt
@@ -0,0 +1,17 @@
+package za.co.varl.trading.service
+
+import za.co.varl.trading.entities.Order
+import za.co.varl.trading.entities.OrderBook
+import za.co.varl.trading.entities.Trade
+import za.co.varl.trading.payload.request.CreateOrderRequest
+
+interface OrderService {
+    fun createLimitOrder(request: CreateOrderRequest): Order
+    fun getOrderById(id: String): Order?
+    fun getAllOrders(): List<Order>
+    fun matchOrders(pair: String)
+    fun getOrderBook(pair: String): OrderBook
+    fun getRecentTrades(pair: String): List<Trade>
+    fun getOpenOrders(pair: String): List<Order>
+    fun getOpenOrdersByCustomerId(customerOrderId: String): List<Order>
+}
