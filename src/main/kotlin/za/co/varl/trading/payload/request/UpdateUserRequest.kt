784fc24173c833f972dc52f0b40cb82c479d7f4d
emagodi <magodiedwin@gmail.com>
2025-02-15 14:49:07 +0200

    Add UpdateUserRequest payload

---
+++ src/main/kotlin/za/co/varl/trading/payload/request/UpdateUserRequest.kt
@@ -0,0 +1,25 @@
+package za.co.varl.trading.payload.request
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import za.co.varl.trading.enums.EmploymentStatus
+import za.co.varl.trading.enums.Purpose
+import za.co.varl.trading.enums.Role
+import za.co.varl.trading.enums.SourceOfFunds
+
+data class UpdateUserRequest(
+    @JsonProperty("firstName") val firstName: String,
+    @JsonProperty("lastName") val lastName: String,
+    @JsonProperty("password") val password: String,
+    @JsonProperty("dateOfBirth") val dateOfBirth: String,
+    @JsonProperty("residentialCountry") val residentialCountry: String,
+    @JsonProperty("identityIssuingCountry") val identityIssuingCountry: String,
+    @JsonProperty("identityType") val identityType: String,
+    @JsonProperty("identityNumber") val identityNumber: String,
+    @JsonProperty("identityExpiryDate") val identityExpiryDate: String,
+    @JsonProperty("cellNumber") val cellNumber: String,
+    @JsonProperty("email") val email: String,
+    @JsonProperty("purpose") val purpose: Purpose,
+    @JsonProperty("employmentStatus") val employmentStatus: EmploymentStatus,
+    @JsonProperty("sourceOfFunds") val sourceOfFunds: SourceOfFunds,
+    @JsonProperty("role") val role: Role
+)
