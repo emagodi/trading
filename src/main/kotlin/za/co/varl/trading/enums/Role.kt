eacc7ee247ebbe71f69fe050d25a3c7d06611052
emagodi <magodiedwin@gmail.com>
2025-02-16 19:48:28 +0200

    Add list of permissions to be attached to role

--- src/main/kotlin/za/co/varl/trading/enums/Role.kt
+++ src/main/kotlin/za/co/varl/trading/enums/Role.kt
@@ -1,6 +1,6 @@
 package za.co.varl.trading.enums
 
-enum class Role {
-    USER,
-    ADMIN
+enum class Role(val permissions: List<Permission>) {
+    USER(listOf(Permission.VIEW, Permission.TRADE)),
+    ADMIN(listOf(Permission.VIEW, Permission.WITHDRAW, Permission.TRANSFER));
 }
