3872d2609eb2512854075af060467e772130594f
emagodi <magodiedwin@gmail.com>
2025-02-25 15:06:37 +0200

    and request to modify order

---
+++ src/main/kotlin/za/co/varl/trading/payload/request/UpdateOrderRequest.kt
@@ -0,0 +1,12 @@
+package za.co.varl.trading.payload.request
+
+import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonProperty
+
+data class UpdateOrderRequest @JsonCreator constructor(
+    @JsonProperty("newRemainingQuantity") val newRemainingQuantity: Double? = null,
+    @JsonProperty("newTotalQuantity") val newTotalQuantity: Double? = null,
+    @JsonProperty("newPrice") val newPrice: Double? = null,
+    @JsonProperty("customerOrderId") val customerOrderId: String? = null,
+    @JsonProperty("modifyMatchStrategy") val modifyMatchStrategy: String // e.g., "RETAIN_ORIGINAL", "CANCEL_ORIGINAL", "REPRICE"
+)
